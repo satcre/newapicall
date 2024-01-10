85aa71b29226c7a3f9bee5c9f83f8034e6423916
Satchit S <satchit.reverseretail@gmail.com>
2024-01-10 12:44:37 +0530

    Updated function

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-FunctionsFramework.http 'call_api' do |request|
+FunctionsFramework.http 'call_new_api' do |request|
   uri = URI('https://jsonplaceholder.typicode.com/posts')
   response = Net::HTTP.get(uri)
   { status: 200, content_type: 'application/json', body: JSON.generate({ data: response }) }

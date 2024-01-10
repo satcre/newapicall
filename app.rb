fbe78f4733a093f165acf70965cb62ce45313831
Satchit S <satchit.reverseretail@gmail.com>
2024-01-10 12:34:55 +0530

    Initial commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+# app.rb
+require 'functions_framework'
+require 'net/http'
+require 'uri'
+require 'json'
+
+FunctionsFramework.http 'call_api' do |request|
+  uri = URI('https://jsonplaceholder.typicode.com/posts')
+  response = Net::HTTP.get(uri)
+  { status: 200, content_type: 'application/json', body: JSON.generate({ data: response }) }
+end
